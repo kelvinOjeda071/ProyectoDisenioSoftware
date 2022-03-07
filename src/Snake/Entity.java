cf961203796eac5f484d94b1f909213b6e7b604b
KevinR08 <79124824+KevinR08@users.noreply.github.com>
2022-03-06 22:16:41 -0500

    GSnake_vfinal

---
+++ src/Snake/Entity.java
@@ -0,0 +1,57 @@
+package Snake;
+
+import java.awt.Color;
+import java.awt.Point;
+import java.util.Iterator;
+import java.util.Random;
+
+public abstract class Entity extends Thread {
+
+    Board board;
+    Crash crash;
+    Snake snake;
+    Enemy enemy;
+    int size = 10;
+    int speed = 10;
+    Color color = Color.BLUE;
+    static final Random r = new Random();
+    int count = 0;
+
+    public abstract void generate();
+
+    public abstract void drop();
+
+    public void eatFood(Point n) {
+        Iterator<Point> i = board.food.food.iterator();
+        while (i.hasNext()) {
+            Point food = i.next();
+            if (food.distance(n) < 20) {
+                i.remove();
+                count++;
+                size++;
+            }
+        }
+        board.repaint();
+        //System.out.println("Puntaje por Comida  ent"+count);
+    }
+
+    public int score() {
+        return count;
+    }
+
+    public Point calculateCoord(Point last, Point mouse) {
+        double degree = 0;
+        if (last.x < mouse.x && last.y < mouse.y) {
+            degree = 360 - Math.toDegrees(Math.atan((double) (mouse.y - last.y) / (mouse.x - last.x)));
+        } else if (last.x > mouse.x && last.y > mouse.y) {
+            degree = 180 - Math.toDegrees(Math.atan((double) (last.y - mouse.y) / (last.x - mouse.x)));
+        } else if (last.y > mouse.y && last.x < mouse.x) {
+            degree = Math.toDegrees(Math.atan((double) (last.y - mouse.y) / (mouse.x - last.x)));
+        } else if (last.y < mouse.y && last.x > mouse.x) {
+            degree = 180 + Math.toDegrees(Math.atan((double) (mouse.y - last.y) / (last.x - mouse.x)));
+        }
+        Point p = new Point((int) (last.x + Math.cos(Math.toRadians(degree)) * speed), (int) (last.y - Math.sin(Math.toRadians(degree)) * speed));
+        return p;
+    }
+
+}
---
+++ src/Snake/Game.java
@@ -0,0 +1,58 @@
+package Snake;
+
+import static Snake.Snake.r;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.PointerInfo;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Game {
+    Board board;
+    Snake snake; 
+    Food food;
+    Enemy enemy;
+    Entity entity;
+    public void startGame(){
+        snake = new Snake();    
+        board = new Board("Slither.io");
+        food = new Food();
+        enemy = new Enemy();
+
+        board.snake = snake; 
+        snake.board = board;
+        
+        board.food = food; 
+        
+        board.enemy = enemy;
+        enemy.board = board;
+        
+        snake.snake = new ArrayList<>();
+        enemy.enemy = new ArrayList<>();
+        food.food = new ArrayList<>();
+        
+        snake.snake.add(new Point(500, 500));
+        enemy.enemy.add(new Point(r.nextInt(900), r.nextInt(900)));
+
+        snake.start();
+        food.start();
+        enemy.start();
+        //finishGame();
+        while(true){
+             getScore();
+        }
+       
+    }
+    
+    public void getScore(){
+        System.out.println("Puntaje por comida" + snake.score());
+    }
+    
+    /*
+    public void finishGame()
+        board.controlSize();
+    }*/
+    
+}
